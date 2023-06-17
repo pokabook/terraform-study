55bebffb1e341aa288aa2fe8ebd7b0a038a3d0bc
poka <khs0826@dsm.hs.kr>
2023-06-18 01:26:54 +0900

    variable 추가

--- tf-eks/main.tf
+++ tf-eks/main.tf
@@ -1,12 +1,12 @@
 module "vpc" {
   source   = "./modules/vpc"
-  vpc_name = "poka-eks"
+  vpc_name = "${var.name}-vpc"
   azs      = slice(data.aws_availability_zones.zones.names, 0, 3)
 }
 
 module "eks" {
   source          = "./modules/eks-cluster"
-  cluster_name    = "fast-cluster"
+  cluster_name    = "${var.name}-cluster"
   cluster_version = "1.24"
   vpc_id          = module.vpc.vpc_id
 
