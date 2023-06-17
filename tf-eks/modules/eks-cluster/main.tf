f827ccb02ad0d3005cdc38bfab8dd0b9b0b2d5c3
poka <khs0826@dsm.hs.kr>
2023-06-18 04:42:56 +0900

    설정

--- tf-eks/modules/eks-cluster/main.tf
+++ tf-eks/modules/eks-cluster/main.tf
@@ -45,7 +45,6 @@ module "eks" {
     }
   }
 
-  create_aws_auth_configmap = true
   manage_aws_auth_configmap = true
 
   aws_auth_users = [
--- tf-eks/modules/eks-cluster/provider.tf
+++ tf-eks/modules/eks-cluster/provider.tf
@@ -20,3 +20,30 @@ provider "aws" {
   region = "us-east-1"
   alias = "virginia"
 }
+
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+provider "kubectl" {
+  apply_retry_count      = 5
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+  }
+}
