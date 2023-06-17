244d54bb7ce5196ebc8ee9d92395b7e9fa56181d
poka <khs0826@dsm.hs.kr>
2023-06-18 01:23:02 +0900

    정리

--- tf-eks/main.tf
+++ tf-eks/main.tf
@@ -1,14 +1,14 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source   = "./modules/vpc"
   vpc_name = "poka-eks"
-  azs = slice(data.aws_availability_zones.zones.names, 0, 3)
+  azs      = slice(data.aws_availability_zones.zones.names, 0, 3)
 }
 
 module "eks" {
-  source = "./modules/eks-cluster"
-  cluster_name = "fast-cluster"
+  source          = "./modules/eks-cluster"
+  cluster_name    = "fast-cluster"
   cluster_version = "1.24"
-  vpc_id = module.vpc.vpc_id
+  vpc_id          = module.vpc.vpc_id
 
   private_subnets = module.vpc.private_subnet_ids
   public_subnets  = module.vpc.public_subnet_ids
--- tf-eks/provider.tf
+++ tf-eks/provider.tf
@@ -9,6 +9,6 @@ terraform {
 }
 
 provider "aws" {
-  region  = "ap-northeast-2"
+  region = "ap-northeast-2"
 }
 
